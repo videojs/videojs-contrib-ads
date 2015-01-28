271e5334dc4fcb50987a3a5ccd4377c1f8d9e11c
Gary Katsevman <git@gkatsev.com>
2015-01-28 17:20:11 -0500

    whitespace commit

--- example/example-integration.js
+++ example/example-integration.js
@@ -6,7 +6,7 @@
  */
 (function(window, document, vjs, undefined) {
 "use strict";
-  
+
   /**
    * Register the ad integration plugin.
    * To initialize for a player, call player.exampleAds().
@@ -14,11 +14,11 @@
    * @param {mixed} options Hash of obtions for the exampleAds plugin.
    */
   vjs.plugin('exampleAds', function(options){
-    
+
     var
-      
+
       player = this,
-      
+
       // example plugin state, may have any of these properties:
       //  - inventory - hypothetical ad inventory, list of URLs to ads
       //  - lastTime - the last time observed during content playback
@@ -27,10 +27,10 @@
       //  - midrollPlayed - whether we've played a midroll
       //  - postrollPlayed - whether we've played a postroll
       state = {},
-      
+
       // asynchronous method for requesting ad inventory
       requestAds = function() {
-        
+
         // reset plugin state
         state = {};
         
@@ -50,42 +50,42 @@
           }
         };
         xhr.send(null);
-        
+
       },
-      
+
       // play an ad, given an opportunity
       playAd = function() {
-        
+
         // short-circuit if we don't have any ad inventory to play
         if (!state.inventory) {
           return;
         }
-        
+
         // tell ads plugin we're ready to play our ad
         player.ads.startLinearAdMode();
         state.adPlaying = true;
-        
+
         // tell videojs to load the ad
         var media = state.inventory[Math.floor(Math.random() * state.inventory.length)];
         player.src(media);
-        
+
         // when the video metadata is loaded, play it!
         player.one('durationchange', function() {
           player.play();
         });
-        
+
         // when it's finished
         player.one('ended', function() {
           // play your linear ad content, then when it's finished ...
           player.ads.endLinearAdMode();
           state.adPlaying = false;
         });
-        
+
       };
-    
+
     // initialize the ads plugin, passing in any relevant options
     player.ads(options);
-    
+
     // request ad inventory whenever the player gets new content to play
     player.on('contentupdate', requestAds);
     // if there's already content loaded, request an add immediately
@@ -99,7 +99,7 @@
         playAd();
       }
     });
-    
+
     // play an ad the first time there's a preroll opportunity
     player.on('readyforpreroll', function() {
       if (!state.prerollPlayed) {
@@ -107,29 +107,29 @@
         playAd();
       }
     });
-    
+
     // watch for time to pass 15 seconds, then play an ad
     // if we haven't played a midroll already
     player.on('timeupdate', function(event) {
-      
+
       if (state.midrollPlayed) {
         return;
       }
-      
+
       var currentTime = player.currentTime(), opportunity;
-      
+
       if ('lastTime' in state) {
         opportunity = currentTime > 15 && state.lastTime < 15;
       }
-      
+
       state.lastTime = currentTime;
       if (opportunity) {
         state.midrollPlayed = true;
         playAd();
       }
-      
+
     });
-    
+
   });
-  
+
 })(window, document, videojs);
