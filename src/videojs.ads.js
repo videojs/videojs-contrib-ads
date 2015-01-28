daebe5503cf4f57ff06347e4ba1a4d0b484d33d4
Gary Katsevman <git@gkatsev.com>
2015-01-28 17:19:34 -0500

    WIP: contentended

--- src/videojs.ads.js
+++ src/videojs.ads.js
@@ -482,6 +482,15 @@ var
               },
               'adserror': function() {
                 this.state = 'content-playback';
+              },
+              'ended': function() {
+                if (player.ads.ended) {
+                  event.preventDefault();
+                  event.stopImmediatePropagation();
+                  event.stopPropagation();
+                  this.state = 'content-playback';
+                  player.trigger('ended');
+                }
               }
             }
           },
@@ -516,6 +525,16 @@ var
                 } else {
                   this.state = 'ads-ready?';
                 }
+              },
+              'ended': function() {
+                //this.state = 'ad-playback';
+                player.ads.ended = true;
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                event.stopPropagation();
+                setImmediate(function() {
+                  player.trigger('contentended');
+                });
               }
             }
           }
