cda393e51b30660625dfbe0ebb1ad331acabe7fe
ldayananda <lahiru07@gmail.com>
2017-01-03 11:20:09 -0500

    Linting updates

--- src/cueTextTracks.js
+++ src/cueTextTracks.js
@@ -1,5 +1,6 @@
 /**
-* This feature allows metadata text tracks to be manipulated once available, @see processMetadataTracks.
+* This feature allows metadata text tracks to be manipulated once available
+* @see processMetadataTracks.
 * It also allows ad implementations to leverage ad cues coming through
 * text tracks, @see processAdTrack
 **/
@@ -12,7 +13,8 @@ const cueTextTracks = {};
 * This feature allows metadata text tracks to be manipulated once they are available,
 * usually after the 'loadstart' event is observed on the player
 * @param player A reference to a player
-* @param processMetadataTrack A callback that performs some operations on a metadata text track
+* @param processMetadataTrack A callback that performs some operations on a
+* metadata text track
 **/
 cueTextTracks.processMetadataTracks = function(player, processMetadataTrack) {
   const tracks = player.textTracks();
@@ -27,6 +29,7 @@ cueTextTracks.processMetadataTracks = function(player, processMetadataTrack) {
   if (tracks.length > 0) {
     for (let i = 0; i < tracks.length; i++) {
       const track = tracks[i];
+
       setModeAndProcess(track);
     }
   // Wait until text tracks are added
@@ -36,6 +39,7 @@ cueTextTracks.processMetadataTracks = function(player, processMetadataTrack) {
   } else {
     tracks.addEventListener('addtrack', (event) => {
       const track = event.track;
+
       setModeAndProcess(track);
     });
   }
@@ -65,58 +69,11 @@ cueTextTracks.getSupportedAdCue = function(player, cue) {
 /**
 * Gets the id associated with a cue.
 * @param cue The cue to extract an ID from
-* @returns The first occurance of 'id' in the object @override if this is not the desired cue id
+* @returns The first occurance of 'id' in the object,
+* @override if this is not the desired cue id
 **/
 cueTextTracks.getCueId = function(player, cue) {
   return cue.id;
-};
-
-/**
-* This feature allows ad metadata tracks to be manipulated in ad implementations
-* @param player A reference to the player
-* @param cues The set of cues to work with
-* @param processCue A method that uses a cue to make some ad request in the ad implementation
-* @param [cancelAds] A method that dynamically cancels ads in the ad implementation
-**/
-cueTextTracks.processAdTrack = function(player, cues, processCue, cancelAds) {
-  player.ads.includedCues = {};
-
-  // loop over set of cues
-  for (let i = 0; i < cues.length; i++) {
-    const cue = cues[i];
-    let cueData;
-    let cueId;
-    let startTime;
-
-    // Exit if this is not a supported cue
-    cueData = this.getSupportedAdCue(player, cue);
-    if (cueData === -1) {
-      videojs.log.warn('Skipping as this is not a supported ad cue.', cue);
-      return;
-
-    // Continue processing supported cue
-    } else {
-      cueId = this.getCueId(player, cue);
-      startTime = cue.startTime;
-
-      // Skip ad if cue was already used
-      if (cueAlreadySeen(player, cueId)) {
-        videojs.log('Skipping ad already seen with ID ' + cueId);
-        return;
-      }
-
-      // Process cue as an ad cue
-      processCue(player, cueData, cueId, startTime);
-
-      // Indicate that this cue has been used
-      setCueAlreadySeen(player, cueId);
-    }
-
-    // Optional dynamic ad cancellation
-    if (cancelAds !== undefined) {
-      cancelAds(player, cueData);
-    }
-  }
 };
 
 /**
@@ -134,6 +91,51 @@ const cueAlreadySeen = function(player, cueId) {
 const setCueAlreadySeen = function(player, cueId) {
   if (cueId !== undefined && cueId !== '') {
     player.ads.includedCues[cueId] = true;
+  }
+};
+
+/**
+* This feature allows ad metadata tracks to be manipulated in ad implementations
+* @param player A reference to the player
+* @param cues The set of cues to work with
+* @param processCue A method that uses a cue to make some ad request
+* in the ad implementation
+* @param [cancelAds] A method that dynamically cancels ads in the ad implementation
+**/
+cueTextTracks.processAdTrack = function(player, cues, processCue, cancelAds) {
+  player.ads.includedCues = {};
+
+  // loop over set of cues
+  for (let i = 0; i < cues.length; i++) {
+    const cue = cues[i];
+    const cueData = this.getSupportedAdCue(player, cue);
+
+    // Exit if this is not a supported cue
+    if (cueData === -1) {
+      videojs.log.warn('Skipping as this is not a supported ad cue.', cue);
+      return;
+    }
+
+    // Continue processing supported cue
+    const cueId = this.getCueId(player, cue);
+    const startTime = cue.startTime;
+
+    // Skip ad if cue was already used
+    if (cueAlreadySeen(player, cueId)) {
+      videojs.log('Skipping ad already seen with ID ' + cueId);
+      return;
+    }
+
+    // Process cue as an ad cue
+    processCue(player, cueData, cueId, startTime);
+
+    // Indicate that this cue has been used
+    setCueAlreadySeen(player, cueId);
+
+    // Optional dynamic ad cancellation
+    if (cancelAds !== undefined) {
+      cancelAds(player, cueData);
+    }
   }
 };
 
