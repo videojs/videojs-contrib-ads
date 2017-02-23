605d5af2ad413c23987f70e32187d8e850134310
ldayananda <ldayananda@users.noreply.github.com>
2017-02-23 14:09:29 -0500

    Do not take a snapshot when we already know there will not be a postroll (#232)

--- src/plugin.js
+++ src/plugin.js
@@ -440,7 +440,6 @@ const contribAdsPlugin = function(options) {
     },
     'postroll?': {
       enter() {
-        this.snapshot = snapshot.getPlayerSnapshot(player);
         if (player.ads.nopostroll_) {
           window.setTimeout(function() {
             // content-resuming happens after the timeout for backward-compatibility
@@ -450,6 +449,8 @@ const contribAdsPlugin = function(options) {
             player.trigger('ended');
           }, 1);
         } else {
+          // Only take a snapshot when there might be a postroll
+          this.snapshot = snapshot.getPlayerSnapshot(player);
           player.addClass('vjs-ad-loading');
 
           player.ads.adTimeoutTimeout = window.setTimeout(function() {
