c2ec693049d6173e11e1c4838298ad13998973ab
ldayananda <ldayananda@users.noreply.github.com>
2017-03-08 17:54:08 -0500

    Preparing the 4.2.5 release (#246)
    * Revert "Do not take a snapshot when we already know there will not be a postroll (#232)"
    
    This reverts commit 605d5af2ad413c23987f70e32187d8e850134310.
    
    * Updating changelog

--- src/plugin.js
+++ src/plugin.js
@@ -447,6 +447,7 @@ const contribAdsPlugin = function(options) {
     },
     'postroll?': {
       enter() {
+        this.snapshot = snapshot.getPlayerSnapshot(player);
         if (player.ads.nopostroll_) {
           window.setTimeout(function() {
             // content-resuming happens after the timeout for backward-compatibility
@@ -456,8 +457,6 @@ const contribAdsPlugin = function(options) {
             player.trigger('ended');
           }, 1);
         } else {
-          // Only take a snapshot when there might be a postroll
-          this.snapshot = snapshot.getPlayerSnapshot(player);
           player.addClass('vjs-ad-loading');
 
           player.ads.adTimeoutTimeout = window.setTimeout(function() {
