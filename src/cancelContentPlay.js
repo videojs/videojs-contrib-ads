417a9c20737ccc75f9d2f411b720fd0aa665f94f
Alex Barstow <alexander.barstow@gmail.com>
2017-04-26 11:19:12 -0400

    Placeholder div's CSS position and top values now match the player's (#253)
    * added position and top values for placeholder div, wip
    
    * edited comment
    
    * placeholder position and top values now come from players computed styles
    
    * fixed getComputedStyle syntax

--- src/cancelContentPlay.js
+++ src/cancelContentPlay.js
@@ -20,8 +20,11 @@ export default function cancelContentPlay(player) {
   if ((videojs.browser.IS_IOS && videojs.browser.IS_IPHONE) &&
       !player.el_.hasAttribute('playsinline')) {
 
+    // The placeholder's styling should match the player's
     const width = player.currentWidth ? player.currentWidth() : player.width();
     const height = player.currentHeight ? player.currentHeight() : player.height();
+    const position = window.getComputedStyle(player.el_).position;
+    const top = window.getComputedStyle(player.el_).top;
 
     // A placeholder black box will be shown in the document while the player is hidden.
     const placeholder = document.createElement('div');
@@ -29,6 +32,8 @@ export default function cancelContentPlay(player) {
     placeholder.style.width = width + 'px';
     placeholder.style.height = height + 'px';
     placeholder.style.background = 'black';
+    placeholder.style.position = position;
+    placeholder.style.top = top;
     player.el_.parentNode.insertBefore(placeholder, player.el_);
 
     // Hide the player. While in full-screen video playback mode on iOS, this
