f4de7fd32ddba627f9335de490ad63cd10900a92
Alex Barstow <abarstow@brightcove.com>
2017-08-07 14:33:09 -0400

    removed whitespace

--- src/plugin.js
+++ src/plugin.js
@@ -175,7 +175,7 @@ const contribAdsPlugin = function(options) {
     // TODO reset state to content-set here instead of in every contentupdate case
     reset() {
       const textTrackList = player.textTracks();
-      
+
       textTrackList.removeEventListener('change', player.ads.snapshot.trackChangeHandler);
       player.ads.disableNextSnapshotRestore = false;
       player.ads._contentEnding = false;
