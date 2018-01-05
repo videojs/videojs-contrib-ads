e3432a0d86a272aa1edea91d7f40f0a3754e3c70
Greg Smith <gsmith@brightcove.com>
2018-01-05 14:55:39 -0500

    finish first pass at BeforePreroll.js

--- src/states/BeforePreroll.js
+++ src/states/BeforePreroll.js
@@ -1,6 +1,21 @@
 import ContentState from './abstract/ContentState.js';
 import Preroll from './Preroll.js';
+import ContentPlayback from './ContentPlayback.js';
+import cancelContentPlay from '../cancelContentPlay.js';
 
+/*
+ * This is the initial state for a player with an ad plugin. Normally, it remains in this
+ * state until a "play" event is seen. After that, we enter the Preroll state to check for
+ * prerolls. This happens regardless of whether or not any prerolls ultimately play.
+ * Errors and other conditions may lead us directly from here to ContentPlayback.
+ *
+ * TODO: The below information is notes for the refactor and can be removed
+ * afterwards.
+ *
+ * Previous states:
+ * * content-set (In new code: BeforePreroll.adsready false)
+ * * ads-ready (In new code: BeforePreroll.adsready true)
+ */
 export default class BeforePreroll extends ContentState {
 
   constructor(player) {
@@ -11,8 +26,7 @@ export default class BeforePreroll extends ContentState {
 
   /*
    * The integration may trigger adsready before the play request. If so,
-   * remember this information so we can get going when the play request
-   * comes in.
+   * we record that adsready already happened so the Preroll state will know.
    */
   onAdsReady() {
     this.adsReady = true;
@@ -20,10 +34,48 @@ export default class BeforePreroll extends ContentState {
 
   /*
    * Ad mode officially begins on the play request, because at this point
-   * content playback is officially blocked by the ad plugin.
+   * content playback is blocked by the ad plugin.
    */
   onPlay() {
-    this.player.ads.stateInstance = new Preroll(this.player, this.adsReady);
+    const player = this.player;
+
+    this.player.ads.stateInstance = new Preroll(player, this.adsReady);
+
+    cancelContentPlay(player);
+
+    // This removes the poster so it doesn't flash between videos.
+    // TODO This is only invoked if adsReady is false to match the pre-refactor
+    // implementation. We should investigate if that is necessary.
+    if (this.adsReady === false) {
+      player.ads.removeNativePoster();
+    }
+  }
+
+  /*
+   * TODO The adscanceled event seems to be redundant. We should consider removing it.
+   * skipLinearAdMode does the same thing, but in a more robust way.
+   */
+  onAdsCanceled() {
+    // TODO the check for adsReady is only to match the pre-refactor behavior.
+    // It is probably unnecessary at best, a bug at worst. It should be investigated
+    // and hopefully removed.
+    if (this.adsReady === false) {
+      this.player.ads.stateInstance = new ContentPlayback(this.player);
+    }
+  }
+
+  /*
+   * An ad error occured. Play content instead.
+   */
+  onAdsError() {
+    this.player.ads.stateInstance = new ContentPlayback(this.player);
+  }
+
+  /*
+   * Ad mode skipped by integration. Play content instead.
+   */
+  skipLinearAdMode() {
+    this.player.ads.stateInstance = new ContentPlayback(this.player);
   }
 
 }
