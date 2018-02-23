fa218e411cbc4749442961e21557e0261b39ac6f
Greg Smith <gsmith@brightcove.com>
2018-02-23 16:05:27 -0500

    remove bower because it messes up conventional-changelog

--- scripts/version.js
+++ scripts/version.js
@@ -8,37 +8,11 @@ const pkg = require('../package.json');
 
 process.chdir(path.resolve(__dirname, '..'));
 
-/*
- * Determines whether or not the project has the Bower setup by checking for
- * the presence of a bower.json file.
- *
- * @return {Boolean}
- */
-const hasBower = () => {
-  try {
-    fs.statSync('./bower.json');
-    return true;
-  } catch (x) {
-    return false;
-  }
-};
-
 const commands = [];
 
 if (!semver.prerelease(pkg.version)) {
   commands.push('conventional-changelog -p videojs -i CHANGELOG.md -s');
   commands.push('git add CHANGELOG.md');
-}
-
-// If the project supports Bower, perform special extra versioning step.
-if (hasBower()) {
-  commands.push('git add package.json');
-  commands.push(`git commit -m "${pkg.version}"`);
-
-  // We only need a build in the Bower-supported case because of the
-  // temporary addition of the dist/ directory.
-  commands.push('npm run build');
-  commands.push('git add -f dist');
 }
 
 if (commands.length) {
