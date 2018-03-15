4745e71e050218100000b0a2b5e2559075e69996
Greg Smith <gsmith@brightcove.com>
2018-03-15 18:25:21 -0400

    update comment to be consistent

--- test/test.ads.js
+++ test/test.ads.js
@@ -385,7 +385,7 @@ QUnit.test('an "ended" event is fired after postroll if not fired naturally', fu
 
   this.player.ads.startLinearAdMode(); // start postroll
   this.player.ads.endLinearAdMode();
-  this.player.trigger('adended'); // simulate snapshot ending
+  this.player.trigger('adended'); // Snapshot restore is done
   assert.strictEqual(endedSpy.callCount, 1, 'ended event happened');
 });
 
