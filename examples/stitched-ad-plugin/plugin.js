c13cc0a3416a3240f9140a34b2f3fd69003e79dd
Pat O'Neill <pgoneill@gmail.com>
2018-08-08 13:51:39 -0400

    Rework example

--- examples/stitched-ad-plugin/plugin.js
+++ examples/stitched-ad-plugin/plugin.js
@@ -1,9 +1,13 @@
 
 // This is an example of a very simple, naive stitched ads plugin. This means
-// there is a single source within which are the ads.
+// there is a single source within which are the "ads".
 //
 // We'll simulate a preroll for the first 5 seconds of playback, a midroll at
 // 15 seconds, and a postroll.
+//
+// In reality, it's left up to the integration to define when and where ad
+// mode is started and ended via some kind of mapping or manifest.
+//
 videojs.registerPlugin('exampleStitchedAds', function(options) {
   var player = this;
 
@@ -12,61 +16,59 @@ videojs.registerPlugin('exampleStitchedAds', function(options) {
   // Initialize contrib-ads.
   player.ads(options);
 
-  var waitForCurrentTime = function(time, fn) {
-    return function waiter() {
-      if (player.currentTime() < time) {
-        return;
-      }
-      player.off(['timeupdate', 'adtimeupdate'], waiter);
-      fn();
-    };
-  };
+  player.on('canplay', function() {
+    player.one('playing', function() {
+      var havePlayedPreroll = false;
+      var haveStartedMidroll = false;
+      var haveStartedPostroll = false;
+      var havePlayedMidroll = false;
 
-  var simulateEndOfAd = function(time) {
-    return waitForCurrentTime(time, function() {
-      player.ads.endLinearAdMode();
-
-      // We need to tell contrib-ads that content has resumed because it
-      // otherwise expects a "play" event.
-      player.trigger('contentresumed');
-    });
-  };
-
-  var simulateStartOfAd = function(time) {
-    return waitForCurrentTime(time, function() {
-      player.on('adtimeupdate', simulateEndOfAd(time + 5));
+      // Simulate a pre-roll immediately upon playback starting.
       player.ads.startLinearAdMode();
+
+      // Simulate a mid-roll at 15 seconds and a post-roll at 5 seconds from
+      // the end. Need to listen to both timeupdate and adtimeupdate because
+      // redispatch will prefix during ads.
+      player.on(['timeupdate', 'adtimeupdate'], function(e) {
+        var currentTime = player.currentTime();
+        var duration = player.duration();
+
+        videojs.log(e.type + ' ' + currentTime + ' of ' + duration);
+
+        // End the pre-roll.
+        if (!havePlayedPreroll && currentTime >= 5) {
+          havePlayedPreroll = true;
+          player.ads.endLinearAdMode();
+          return;
+        }
+
+        // Start the mid-roll.
+        if (!haveStartedMidroll && currentTime >= 15) {
+          haveStartedMidroll = true;
+          player.ads.startLinearAdMode();
+          return;
+        }
+
+        // End the mid-roll.
+        if (!havePlayedMidroll && currentTime >= 20) {
+          havePlayedMidroll = true;
+          player.ads.endLinearAdMode();
+          return;
+        }
+
+        // Start the post-roll. The post-roll will be ended via the `adended` event.
+        if (!haveStartedPostroll && currentTime >= duration - 5) {
+          haveStartedPostroll = true;
+          player.ads.startLinearAdMode();
+          return;
+        }
+      });
     });
-  };
-
-  // When the player is ready for a pre-roll, we'll simulate a 5 second
-  // stitched ad at the start of playback.
-  player.on('readyforpreroll', function() {
-    simulateStartOfAd(0)();
   });
 
-  // Simulate a mid-roll at 15 seconds.
-  player.on('timeupdate', simulateStartOfAd(15));
-
-  // Simulate a post-roll at 5 seconds from the actual end of content.
-  // This is a bit strange. In reality, you'd probably want to use middleware
-  // to adjust the timelines to behave more like you'd expect, but this is a
-  // reasonable approximation.
-  player.one('canplaythrough', function() {
-    player.on('timeupdate', waitForCurrentTime(player.duration() - 5, function() {
-      player.tech(true).trigger('ended');
-      player.play();
-    }));
+  // The actual ended event will now happen during the postroll playback.
+  player.one('adended', function() {
+    player.ads.endLinearAdMode();
+    player.trigger('ended');
   });
-
-  player.on('readyforpostroll', function() {
-    player.ads.startLinearAdMode();
-
-    // The actual ended event will now happen during the postroll playback.
-    player.one('adended', function() {
-      player.ads.endLinearAdMode();
-    });
-  });
-
-  player.trigger('adsready');
 });
